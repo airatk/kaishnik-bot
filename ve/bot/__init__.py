439da9d1fbcb3103c73da87d9603ba8a3ee57e1f
airatk <kamairat@icloud.com>
2019-05-31 18:10:39 +0300

    VE: new launch mode was added

--- ve/bot/__init__.py
+++ ve/bot/__init__.py
@@ -6,11 +6,12 @@ from .sbot import Bot
 def main():
     sbot = Bot()
     
-    sbot.notification.start()
-    
-    if len(argv) > 1:
+    if len(argv) > 1 and argv[1] == "i":
+        sbot.notification.start()
+        sbot.start()
+        sbot.notification.join()
+    elif len(argv) > 1 and argv[1] == "c":
+        print("without notification")
         sbot.start()
     else:
         sbot.test()
-    
-    sbot.notification.join()
